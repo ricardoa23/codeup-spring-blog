22bc40fad1205c4922c764ff2326378b7ff297d7
Ricardo Ayala <ricardo.a.ayala86@gmail.com>
2023-04-04 20:07:27 -0500

    completed some additional assignments

--- src/main/java/com/codeup/codeupspringblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/RollDiceController.java
@@ -2,7 +2,9 @@ package com.codeup.codeupspringblog.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -20,10 +22,17 @@ public class RollDiceController {
 //        int diceRoll = rollDice();
 //
 //    }
-
     public static int rollDice() {
         Random random = new Random();
         return random.nextInt(6) + 1;
+    }
+
+    @PostMapping("/roll-dice/{n}")
+    public String rollAnswer(Model model, int guess) {
+        int roll = rollDice();
+        model.addAttribute("guess", guess);
+        model.addAttribute("roll", roll);
+        return"roll-dice-answer";
     }
 
 
